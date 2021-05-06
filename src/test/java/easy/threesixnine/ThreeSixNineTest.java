0bd164212ac73f31a9a0cae84aac777e36d4b969
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-05-06 19:24:49 +0200

    #2 Easy - ThreeSixNine refactoring Arrays.equals in test case

--- src/test/java/easy/threesixnine/ThreeSixNineTest.java
+++ src/test/java/easy/threesixnine/ThreeSixNineTest.java
@@ -12,6 +12,7 @@ class ThreeSixNineTest {
     void solve() {
         int input = 16;
         String[] expected = {"1", "2", "clap", "4", "5", "clap", "7", "8", "clap", "10", "11", "clap", "clap", "14", "clap", "clap"};
-        assertTrue(Arrays.toString(new ThreeSixNine().solve(input)).equals(Arrays.toString(expected)));
+        //assertTrue(Arrays.toString(new ThreeSixNine().solve(input)).equals(Arrays.toString(expected)));
+        assertTrue(Arrays.equals(expected, new ThreeSixNine().solve(input)));
     }
 }
