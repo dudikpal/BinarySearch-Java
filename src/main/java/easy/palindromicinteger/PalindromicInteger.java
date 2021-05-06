ef9f203d71e32f0685bbd8bfe68d3c9ab013a61c
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-05-06 17:27:34 +0200

    #1 Easy - PalindromicInteger done

--- src/main/java/easy/palindromicinteger/PalindromicInteger.java
+++ src/main/java/easy/palindromicinteger/PalindromicInteger.java
@@ -1,4 +1,4 @@
-package palindromicinteger;
+package easy.palindromicinteger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/test/java/easy/palindromicinteger/PalindromicIntegerTest.java
+++ src/test/java/easy/palindromicinteger/PalindromicIntegerTest.java
@@ -1,4 +1,4 @@
-package palindromicinteger;
+package easy.palindromicinteger;
 
 import org.junit.jupiter.api.Test;
 
