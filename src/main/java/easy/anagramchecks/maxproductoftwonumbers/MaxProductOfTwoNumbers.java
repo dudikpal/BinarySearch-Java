94a2f50f20d623f7a3bec03c52a897973d6c4f55
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-05-09 11:40:48 +0200

    #1 Easy - MaxProductOfTwoNumbers done

---
+++ src/main/java/easy/anagramchecks/maxproductoftwonumbers/MaxProductOfTwoNumbers.java
@@ -0,0 +1,28 @@
+package easy.anagramchecks.maxproductoftwonumbers;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MaxProductOfTwoNumbers {
+
+    public int solve(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        List<Integer> numbers = Arrays.stream(nums)
+                .mapToObj(Integer::valueOf)
+                //.map(Math::abs)
+                .collect(Collectors.toList());
+        numbers.sort(Comparator.naturalOrder());
+        int twoSmallest = numbers.get(0) * numbers.get(1);
+        int twoLargest = numbers.get(numbers.size() - 2) * numbers.get(numbers.size() - 1);
+
+        return Math.max(twoLargest, twoSmallest);
+    }
+}
---
+++ src/test/java/easy/anagramchecks/maxproductoftwonumbers/MaxProductOfTwoNumbersTest.java
@@ -0,0 +1,37 @@
+package easy.anagramchecks.maxproductoftwonumbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxProductOfTwoNumbersTest {
+
+    @Test
+    void test35() {
+        int[] input = {5, 7, 1};
+        int expected = 35;
+        assertEquals(expected, new MaxProductOfTwoNumbers().solve(input));
+    }
+
+    @Test
+    void test49() {
+        int[] input = {7, 1, 7};
+        int expected = 49;
+        assertEquals(expected, new MaxProductOfTwoNumbers().solve(input));
+    }
+
+    @Test
+    void test1() {
+        int[] input = {-1, -1, 0};
+        int expected = 1;
+        assertEquals(expected, new MaxProductOfTwoNumbers().solve(input));
+    }
+
+    @Test
+    void testminus6() {
+        int[] input = {2, -3};
+        int expected = -6;
+        assertEquals(expected, new MaxProductOfTwoNumbers().solve(input));
+    }
+
+}
